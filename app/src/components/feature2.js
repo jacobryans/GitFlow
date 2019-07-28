9ae65fd2afde4ceeb0b07bd3e99438994d216a1f
Robert Gant <demontegant@gmail.com>
2019-07-28 12:39:25 -0500

    having fun function

---
+++ app/src/components/feature2.js
@@ -0,0 +1,14 @@
+import react from 'react';
+
+const HavingFun = () => {
+  return (
+    <form>
+      First name:
+      <input type="text" />
+      Are you having fun:
+      <input type="text" />
+    </form>
+  );
+};
+
+export default HavingFun;
