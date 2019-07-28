ed4ef521c05867b8628257886e893b338a0799ff
renegade27 <foemaster@live.com>
2019-07-28 12:45:32 -0500

    Merge f1 into f2.

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import feature1 from './components/feature1.jsx';
+import feature2 from './components/feature2.jsx';
 import './App.css';
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Route exact path="/feature1" component={feature1} />
-        <Route exact path="/feature2" component={} />
+        <Route exact path="/feature2" component={feature2} />
       </header>
     </div>
   );
--- app/src/components/feature2.jsx
+++ app/src/components/feature2.jsx
@@ -1,10 +1,11 @@
-import react from 'react';
+import React from 'react';
 
 const HavingFun = () => {
   return (
     <form>
       First name:
       <input type="text" />
+      <br />
       Are you having fun:
       <input type="text" />
     </form>
