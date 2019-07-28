5b9d5e220bc5aee5e4093f283d605773467b428c
renegade27 <foemaster@live.com>
2019-07-28 13:00:36 -0500

    New feature3
    Co-authored-by: Robert <demontegant@gmail.com>

--- app/src/App.js
+++ app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import feature1 from './components/feature1.jsx';
 import feature2 from './components/feature2.jsx';
+import feature3 from './components/feature3.jsx';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <header className="App-header">
         <Route exact path="/feature1" component={feature1} />
         <Route exact path="/feature2" component={feature2} />
+        <Route exact path="/feature3" component={feature3} />
       </header>
     </div>
   );
